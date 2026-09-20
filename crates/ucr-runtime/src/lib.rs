aeecc4194cd0cff479d090fb94c1522e57881821
BusinesMetro <mailsvb2@gmail.com>
2026-09-20 22:35:16 +0300

    style: format runtime realtime imports

--- crates/ucr-runtime/src/lib.rs
+++ crates/ucr-runtime/src/lib.rs
@@ -9,10 +9,9 @@ use ucr_api_grpc::{
     GrpcCallService, GrpcConferenceService, GrpcDeviceService, GrpcEventService, GrpcGroupService,
     GrpcIntegrationService, GrpcRealtimeService, GrpcStoreForwardService, GrpcSyncService,
     GrpcUniversalConferenceService, RealtimeWebRtcDependencies, call_service_server,
-    conference_service_server,
-    device_service_server, event_service_server, group_service_server, integration_service_server,
-    realtime_service_server, store_forward_service_server, sync_service_server,
-    universal_conference_service_server,
+    conference_service_server, device_service_server, event_service_server, group_service_server,
+    integration_service_server, realtime_service_server, store_forward_service_server,
+    sync_service_server, universal_conference_service_server,
 };
 use ucr_conference::ConferenceRuntimeState;
 use ucr_core::{
