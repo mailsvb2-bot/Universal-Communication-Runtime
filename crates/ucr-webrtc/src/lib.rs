80a41d06367fcc93e45ae7faeca150e712edcc45
BusinesMetro <mailsvb2@gmail.com>
2026-09-20 23:16:34 +0300

    webrtc: export E2EE SFU bridge wire contract

--- crates/ucr-webrtc/src/lib.rs
+++ crates/ucr-webrtc/src/lib.rs
@@ -1,5 +1,11 @@
 #![forbid(unsafe_code)]
 
+mod e2ee_bridge;
+pub use e2ee_bridge::{
+    MAX_WEBRTC_E2EE_WIRE_BYTES, WEBRTC_E2EE_DATA_CHANNEL_LABEL, WebRtcE2eeIngressFrame,
+    WebRtcE2eeWireError, decode_webrtc_e2ee_envelope, encode_webrtc_e2ee_envelope,
+};
+
 use core::fmt;
 use std::{
     collections::HashMap,
